6ecae4a92842e7b6b54a225e1adc28b3c1ff75bb
raulbr99 <47525849+raulbr99@users.noreply.github.com>
2024-05-16 21:15:14 +0200

    Add sitemaps

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,10 +2,12 @@ import { defineConfig } from 'astro/config';
 import tailwind from "@astrojs/tailwind";
 import vercel from '@astrojs/vercel/serverless';
 import robotsTxt from "astro-robots-txt";
+import sitemap from '@astrojs/sitemap';
+
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), robotsTxt()],
+  integrations: [tailwind(), robotsTxt(), sitemap()],
   site: 'https://porfolio-rauldev99.vercel.app/',
   output: 'server',
   adapter: vercel({
